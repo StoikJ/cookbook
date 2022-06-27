52393c1c790c822609bb38e9293fae239999ad35
Jeff Schutrop <Jeffschutrop19751975@gmail.com>
2022-06-27 17:38:03 -0500

    added tailwind and also styles

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className=" w-screen text-white text-3xl font-bold underline bg-zinc-900 ">
       <h1>Cook Book </h1>
     </div>
   );
