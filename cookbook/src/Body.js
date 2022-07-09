4b742836c20fbedec530511efb7c8dfa03af0d91
Jamirah <64516854+Jamirah@users.noreply.github.com>
2022-07-09 09:25:39 -0400

    commented out body.js to trouble shoot and stop it from failing to compile in case other members need to do work on main branch- will continue to work on this issue on my seperate branch.

--- cookbook/src/Body.js
+++ cookbook/src/Body.js
@@ -1,38 +1,37 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
+//import React, { useEffect, useState } from "react";
+//import axios from "axios";
+//import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
-const [items, setitems] = useState([]);
+//const [items, setitems] = useState([]);
 
-function Body() {
-  useEffect(() => {
-    axios
-      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-      .then((res) => {
-        console.log(res.data);
+//function Body() {
+  //useEffect(() => {
+    //axios
+      //.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      //.then((res) => {
+        //console.log(res.data);
 
-        setitems(res.data.meals);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+        //setitems(res.data.meals);
+      //})
+      //.catch((err) => {
+        //console.log(err);
+     // });
+  //}, []);
 
-  const itemsist=items.map((obj) => {
-    return (
-      <div className="col-md-4">
-        <p>{obj.strMeal}</p>
-        <img src={obj.strMealThumb} className="img-fluid" />
-        <p>{obj.idMeal}</p>
-      </div>
-    );
-  });
+  //const itemsist=items.map((obj) => {
+   // return (
+     //  <p>{obj.strMeal}</p>
+       // <img src={obj.strMealThumb} className="img-fluid" />
+        //<p>{obj.idMeal}</p>
+      //</div>
+    //);
+  //});
 
-  return (
-    <div>
-      <div className="row">{itemslist}</div>
-    </div>
-  );
-}
+  //return (
+    //<div>
+      //<div className="row">{itemslist}</div>
+    //</div>
+  //);
+//}
 
-export default Body;
+//export default Body;
