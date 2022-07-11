8b927ee618cff94eb2a1955985272e514b8a8278
stoikJ <jmoore116@gmail.com>
2022-07-10 17:39:26 -0700

    login page and tailwind

--- cookbook/src/App.js
+++ cookbook/src/App.js
@@ -1,11 +1,34 @@
-import "./App.css";
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignupPage from './pages/Signup';
+import LoginPage from './pages/Login';
 
+function App() {
+  return (
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8">
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </div>
+  );
+}
+
+/*export default App;
 function App() {
   return (
     <div className=" w-screen text-white text-3xl font-bold underline bg-zinc-900 ">
       <h1>Cook Book </h1>
     </div>
   );
-}
+}*/
 
 export default App;
