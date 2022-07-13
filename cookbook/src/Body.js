5d234bfa330356e0e79c5c35171c849428db0b88
Jamirah <64516854+Jamirah@users.noreply.github.com>
2022-07-13 13:35:02 -0400

    ran a npm ic and npm audit fix --force so that webpage for recipes could load- successfully removed all bootstrap html

--- cookbook/src/Body.js
+++ cookbook/src/Body.js
@@ -1,26 +1,37 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
-
-const [items, setitems] = useState([]);
 
 function Body() {
+    const[items, setitems]= useState([])
+
   useEffect(() => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
       .then((res) => {
         console.log(res.data);
+
+        setitems(res.data.meals)
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
-  return (
-    <div>
-      <h1>This is Body</h1>
-    </div>
-  );
+  const itemslist=items.map((obj)=>{
+
+        return<div className='col-md-4'>
+            <p>{obj.strMeal}</p>
+            <img src={obj.strMealThumb} className='img-fluid' />
+            <p>{obj.idMeal}</p>
+        </div>
+  })
+
+  return <div>
+      <div className='row'>
+          {itemslist}
+
+      </div>
+  </div>;
 }
 
 export default Body;
