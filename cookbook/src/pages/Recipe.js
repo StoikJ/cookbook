e2f662bf6cbae7f1b0e20e67a71f7d28030ee70b
stoikJ <jmoore116@gmail.com>
2022-07-18 07:47:35 -0700

    recipe page click stuff

--- cookbook/src/pages/Recipe.js
+++ cookbook/src/pages/Recipe.js
@@ -29,17 +29,19 @@ export default function RecipePage(){
     }
     
     return (
-      <div className="App">
-        <form className="mt-8 space-y-6" onSubmit={getSearch}  >
-          <input className="search-bar" type="text" value={search}
+      <div class="App h-screen">
+        <form class="mt-8 space-y-6" onSubmit={getSearch}  >
+          <input class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+           type="text" value={search}
                onChange={updateSearch} />
-          <button className="search-button" type="submit" >
+          <button class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit" >
                Search
           </button>
         </form>
-        <div className="recipes">
+        <div class="">
           {recipes.map(recipe => (
             <Recipe
+              url={recipe.recipe.url}
               key={recipe.recipe.label}
               title={recipe.recipe.label}
               calories={recipe.recipe.calories}
